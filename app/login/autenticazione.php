1ce09354ccbd93c12457d3c1dc58692363b08822
archistico <info@archistico.com>
2020-09-21 11:42:58 +0200

    add js hash password login

--- app/login/autenticazione.php
+++ app/login/autenticazione.php
@@ -10,6 +10,11 @@ class Autenticazione
 
     public function VerificaPIN($f3)
     {
-        echo "verifica PIN";
+        $password_hash = $f3->get('POST.password_hash');
+        if($password_hash == "b62dc1e428373736775e75e2e922dcd00184c968fc37867024b9b2e30a1f2dffcabad317e4dbe997f3f8d4b38192d436b9e7b3d1351e6f71d781776d955751e5") {
+            $f3->reroute('@comoodo_nuovo_datigenerali');
+        } else {
+            $f3->reroute('@login_autenticazione_richiestapin');
+        }
     }
 }
--- tmp/j6wc2ffcgezd.2j95isnidiwwg.php
+++ tmp/j6wc2ffcgezd.2j95isnidiwwg.php
@@ -6,10 +6,23 @@
     <title>Comoodo - Login</title>
 </head>
 <body>
-    <img src="ui/img/logo.jpg" alt="Logo Comoodo" />
-    <form action="<?= (Base::instance()->alias('login_autenticazione_verificapin')) ?>" method="POST">
-        <input type="password" name="password" id="password" placeholder="Inserisci password">
-        <input type="submit" value="ENTRA">
+    <img src="<?= ($BASE) ?>/ui/img/logo.jpg" alt="Logo Comoodo" />
+    <form action="<?= ($BASE) ?><?= (Base::instance()->alias('login_autenticazione_verificapin')) ?>" method="POST">
+        <input type="password" name="password" id="password" placeholder="Inserisci password" onkeydown="if (event.keyCode == 13) { formhash(this.form, this.form.password); this.form.submit(); return false;}">
+        <input type="submit" value="ENTRA" onclick="formhash(this.form, this.form.password);">
     </form>
+    <script src="<?= ($BASE) ?>/ui/js/hashes.min.js"></script>
+    <script>
+        function formhash(form, password) {
+            var password_hash = document.createElement("input");
+            form.appendChild(password_hash);
+            password_hash.name = "password_hash";
+            password_hash.type = "hidden"
+            var SHA512 = new Hashes.SHA512;
+            password_hash.value = SHA512.hex(password.value);
+            password.value = "";
+            form.submit();
+        }
+    </script>
 </body>
 </html>
